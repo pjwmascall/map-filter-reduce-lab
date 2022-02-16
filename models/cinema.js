a2ce76d1dcb35844d9f4ddb5de60bb3031ae0ab1
Paul Mascall <pjwmascall@hotmail.com>
2022-02-16 14:59:57 +0000

    finished extensions

--- models/cinema.js
+++ models/cinema.js
@@ -17,4 +17,45 @@ Cinema.prototype.getFilmTitle = function(title){
   return filmTitle[0]
 }
 
+Cinema.prototype.getFilmsByProperty = function(value, property){
+  const output = this.films.filter((film) => {
+    if(film[property] === value) {
+      return film
+    }
+  })
+  return output
+}
+
+Cinema.prototype.filmsFromYear = function(year){
+  return this.films.reduce((truthValue, film) => {
+    if (film.year === year) {
+      truthValue = true
+    }
+    return truthValue
+  }, false)
+}
+
+Cinema.prototype.existsFilmsFromYear = function(year){
+  return this.filmsFromYear(year);
+}
+
+Cinema.prototype.notExistsFilmsFromYear = function(year){
+  return !this.filmsFromYear(year);
+}
+
+Cinema.prototype.allFilmsOverLength = function(length){
+  return this.films.reduce((truthValue, film) => {
+    if (film.length > length) {
+      truthValue = true
+    }
+    return truthValue
+  }, false)
+}
+
+Cinema.prototype.totalRunningTime = function() {
+  return this.films.reduce((runningTotal, film) => {
+    return runningTotal + film.length
+  }, 0)
+}
+
 module.exports = Cinema;
--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -39,11 +39,32 @@ describe('Cinema', function () {
     assert.deepStrictEqual(actual, moonlight)
   });
 
+  it('should be able to filter films by genre', function() {
+    const actual = cinema.getFilmsByProperty('drama', 'genre');
+    assert.deepStrictEqual(actual, [moonlight, trainspotting]);
+  });
 
-  it('should be able to filter films by genre');
-  it('should be able to check whether there are some films from a particular year');
-  it('should be able to check whether there are no films from a particular year');
-  it('should be able to check whether all films are over a particular length');
-  it('should be able to calculate total running time of all films');
+
+  it('should be able to check whether there are some films from a particular year', function() {
+    const actual = cinema.existsFilmsFromYear(2017)
+    assert.strictEqual(actual, true)
+  });
+
+
+  it('should be able to check whether there are no films from a particular year', function() {
+    const actual = cinema.notExistsFilmsFromYear(1928)
+    assert.strictEqual(actual, true)
+  });
+
+  it('should be able to check whether all films are over a particular length', function() {
+    const length = 30;
+    const actual = cinema.allFilmsOverLength(length)
+    assert.strictEqual(actual, true)
+  });
+
+  it('should be able to calculate total running time of all films', function() {
+    const actual = cinema.totalRunningTime()
+    assert.strictEqual(actual, 622)
+  });
 
 });
